393f0e7aeb1ec5e924ccd32f140854509588dad4
joel.js <joel.js@inapp.com>
2024-11-08 11:50:22 +0530

    media url

--- src/Video/VideoHome.jsx
+++ src/Video/VideoHome.jsx
@@ -32,7 +32,8 @@ const VideoHome = () => {
         // src: "https://sitesecuritysystems.net:7001/media/761e568f-056b-3cd0-b86c-27375404ef5d.mpegts?auth=ZGFyc2huYS52QGluYXBwLmNvbTpydGZaaXB3ODl2ZXpBWEt1VXlCS1VldVp3eE0yMGc9aHpoamxmOmMwNjI3MTc4MTYzNjg4ZWU1ZTRjNDc2ZDA1MjlmZTJh&pos=2024-11-06T21:31:09.000",
         // src: "https://sitesecuritysystems.net:7001/media/761e568f-056b-3cd0-b86c-27375404ef5d.mp4?auth=ZGFyc2huYS52QGluYXBwLmNvbTpydGZaaXB3ODl2ZXpBWEt1VXlCS1VldVp3eE0yMGc9aHpoamxmOmMwNjI3MTc4MTYzNjg4ZWU1ZTRjNDc2ZDA1MjlmZTJh&pos=2024-11-06T21:31:09.000",
         // src: "https://bitmovin-a.akamaihd.net/webpages/demos/content/multi-codec/hevc/stream_fmp4.m3u8",
-        src: "https://sitesecuritysystems.net:7001/media/52432602-49dd-add2-e448-29a99339e455.mp4?authKey=d2c5aea3-c38f-44c3-83b8-e05a20ee4cea&auth=ZGFyc2huYS52QGluYXBwLmNvbTpydGZaaXB3ODl2ZXpBWEt1VXlCS1VldVp3eE0yMGc9aHpnZWl6OmJhMWU2ZDI5ZGE5MjE0NDg2NGUzMmU3NTJjZjMzY2M1",
+        // src: "https://sitesecuritysystems.net:7001/media/52432602-49dd-add2-e448-29a99339e455.mp4?authKey=d2c5aea3-c38f-44c3-83b8-e05a20ee4cea&auth=ZGFyc2huYS52QGluYXBwLmNvbTpydGZaaXB3ODl2ZXpBWEt1VXlCS1VldVp3eE0yMGc9aHpnZWl6OmJhMWU2ZDI5ZGE5MjE0NDg2NGUzMmU3NTJjZjMzY2M1",
+        src: "https://sitesecuritysystems.net:7001/rest/v4/devices/52432602-49dd-add2-e448-29a99339e455/media.mp4?authKey=d2c5aea3-c38f-44c3-83b8-e05a20ee4cea&auth=ZGFyc2huYS52QGluYXBwLmNvbTpydGZaaXB3ODl2ZXpBWEt1VXlCS1VldVp3eE0yMGc9aHpnZWl6OmJhMWU2ZDI5ZGE5MjE0NDg2NGUzMmU3NTJjZjMzY2M1",
         // type: `application/vnd.apple.mpegurl`,
         // type: "application/x-mpegURL",
         type: 'video/mp4'
